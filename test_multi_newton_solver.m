cc8527ad19a1306c1a70b7648a48013b0ac108ed
AriB2003 <ari.bobesh@gmail.com>
2025-09-19 16:36:36 -0400

    Add up to part 5

---
+++ test_multi_newton_solver.m
@@ -0,0 +1,23 @@
+params = struct();
+params.numerical_diff = 0;
+[roots, flag] = multi_newton_solver(@test_function02, [1;2;3], params)
+check = test_function02(roots)
+
+function [f_out,dfdx] = test_function02(X)
+    x1 = X(1);
+    x2 = X(2);
+    x3 = X(3);
+    y1 = 3*x1^2 + .5*x2^2 + 7*x3^2-100;
+    y2 = 9*x1-2*x2+6*x3;
+    f_out = [y1;y2];
+    dfdx = [6*x1,1*x2,14*x3;9,-2,6];
+end
+
+function f_out = test_function03(X)
+    x1 = X(1);
+    x2 = X(2);
+    x3 = X(3);
+    y1 = 3*x1^2 + .5*x2^2 + 7*x3^2-100;
+    y2 = 9*x1-2*x2+6*x3;
+    f_out = [y1;y2];
+end
